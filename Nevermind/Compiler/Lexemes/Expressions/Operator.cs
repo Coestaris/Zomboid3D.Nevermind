754e65746f36b9c3bad12961c3823eb8e77b4956
Coestaris <vk_vm@ukr.net>
2019-06-03 18:46:26 +0300

    C-like operators

--- Nevermind/Compiler/Lexemes/Expressions/Operator.cs
+++ Nevermind/Compiler/Lexemes/Expressions/Operator.cs
@@ -41,12 +41,14 @@ namespace Nevermind.Compiler.Lexemes.Expressions
             new Operator(new List<TokenType> { TokenType.Tilda                                    }, new Function<IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.MinusSign                                }, new Function<IntegerType, IntegerType>()),
 
-            //Binary
+            //Arithmetical
             new Operator(new List<TokenType> { TokenType.MultiplySign                             }, 13, new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.DivideSign                               }, 13, new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.PercentSign                              }, 12, new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.PlusSign                                 }, 11, new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.MinusSign                                }, 11, new Function<IntegerType, IntegerType, IntegerType>()),
+
+            //Logical comp
             new Operator(new List<TokenType> { TokenType.LessThanSign,    TokenType.LessThanSign  }, 10, new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.GreaterSign,     TokenType.GreaterSign   }, 10, new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.LessThanSign                             }, 9,  new Function<IntegerType, IntegerType, IntegerType>()),
@@ -55,14 +57,29 @@ namespace Nevermind.Compiler.Lexemes.Expressions
             new Operator(new List<TokenType> { TokenType.GreaterSign,     TokenType.EqualSign     }, 9,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.ExclamationMark, TokenType.EqualSign     }, 8,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.EqualSign,       TokenType.EqualSign     }, 8,  new Function<IntegerType, IntegerType, IntegerType>()),
+
+            //Binary operators
             new Operator(new List<TokenType> { TokenType.AmpersandSign                            }, 7,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.CircumflexSign                           }, 6,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.OrSign                                   }, 5,  new Function<IntegerType, IntegerType, IntegerType>()),
+
+            //Logical
             new Operator(new List<TokenType> { TokenType.AmpersandSign,   TokenType.AmpersandSign }, 4,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.OrSign,          TokenType.OrSign        }, 3,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.QuestingSign                             }, 2,  new Function<IntegerType, IntegerType, IntegerType>()),
             new Operator(new List<TokenType> { TokenType.Colon                                    }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
-            new Operator(new List<TokenType> { TokenType.ComaSign                                 }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+
+            //Equal operators
+            new Operator(new List<TokenType> { TokenType.PlusSign,        TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.MinusSign,       TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.MultiplySign,    TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.DivideSign,      TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.AmpersandSign,   TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.CircumflexSign,  TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.OrSign,          TokenType.EqualSign     }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.EqualSign                                }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.EqualSign                                }, 1,  new Function<IntegerType, IntegerType, IntegerType>()),
+            new Operator(new List<TokenType> { TokenType.ComaSign                                 }, 0,  new Function<IntegerType, IntegerType, IntegerType>()),
         };
     }
 }
